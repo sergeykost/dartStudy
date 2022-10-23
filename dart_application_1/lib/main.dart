85da00c592de15824c34df717a93d9e68ee34937
sergeykost <sergey.kostyuchenk0@yandex.ru>
2022-10-23 22:18:41 +0300

    tested some fiches
    :+1:

---
+++ dart_application_1/lib/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('Hello world');
+
+  String name = 'Sergio';
+  String surname = 'Kostyuchenko';
+  print('$name $surname');
+  name = 'Roman';
+  print('$name $surname');
+  
+
+
+
+
+
+
+
+}
